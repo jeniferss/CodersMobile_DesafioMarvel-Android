2a3a770b6e089cb92d33e40ae0964306c47580aa
Jenifer Soares <jey.souza11@gmail.com>
2020-11-28 15:27:15 -0300

    Cria Splash Screen

---
+++ app/src/main/java/com/example/myapplication/desafiomarvelandroid/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.desafiomarvelandroid.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.desafiomarvelandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
