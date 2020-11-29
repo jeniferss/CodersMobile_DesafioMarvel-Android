38cefb96d52bef4ada301d6e47b11f7687f06940
Jenifer Soares <jey.souza11@gmail.com>
2020-11-29 00:24:48 -0300

    Adiciona tela de Registro

--- app/src/main/java/com/example/myapplication/desafiomarvelandroid/login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/desafiomarvelandroid/login/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.myapplication.desafiomarvelandroid.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.myapplication.desafiomarvelandroid.R
+import com.example.myapplication.desafiomarvelandroid.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextView>(R.id.btnCreateAccount).setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/desafiomarvelandroid/register/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/desafiomarvelandroid/register/RegisterActivity.kt
@@ -1,11 +1,18 @@
-package com.example.myapplication.desafiomarvelandroid
+package com.example.myapplication.desafiomarvelandroid.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.desafiomarvelandroid.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
